01c09ba81192b6574ea0ef91232ca715b6098c70
Daisigu <bogd228@mail.ru>
2019-12-28 17:25:51 +0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
-$(function(){
-    $("a[href^='#']").click(function(){
-            var _href = $(this).attr("href");
-            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-            return false;
-    });
+$(document).ready(function(){
+   $('.header__burger').click(function(event){
+    $('.header__burger,.main-header__list').toggleClass('active');
+   });
 });
